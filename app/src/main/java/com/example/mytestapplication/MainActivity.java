c76f9aaa41ce4f5abc42f677e4e76bd26b37f8bc
Fahad <m.fahad@naheed.pk>
2020-02-20 10:55:39 +0500

    3rd

--- app/src/main/java/com/example/mytestapplication/MainActivity.java
+++ app/src/main/java/com/example/mytestapplication/MainActivity.java
@@ -17,56 +17,57 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
     GridLayout gridLayout;
     public static final String URL="";
     InterstitialAd mInterstitialAd;
+    ScheduledExecutorService scheduler;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(!scheduler.isShutdown()){
+            scheduler.shutdown();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        prepareAd();
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
                 Toast.makeText(MainActivity.this, ""+initializationStatus.toString(), Toast.LENGTH_SHORT).show();
             }
         });
-        mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-        mInterstitialAd.setAdListener(new AdListener() {
+
+
+        scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(new Runnable() {
             @Override
-            public void onAdLoaded() {
-                Toast.makeText(MainActivity.this, "onAdLoaded()", Toast.LENGTH_SHORT).show();
+            public void run() {
+                Log.e(this.getClass().getSimpleName(),"Scheduler started...");
+               runOnUiThread(new Runnable() {
+                   @Override
+                   public void run() {
+                       if(mInterstitialAd.isLoaded()){
+                           mInterstitialAd.show();
+                       }else{
+                           Log.e(this.getClass().getSimpleName(),"Interstitial not loaded...");
+                       }
+                       prepareAd();
+                   }
+               });
             }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                Toast.makeText(MainActivity.this,
-                        "onAdFailedToLoad() with error code: " + errorCode,
-                        Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onAdClosed() {
-
-            }
-        });
-        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
-            mInterstitialAd.show();
-        } else {
-            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
-        }
-
-
-        /*mInterstitialAd.loadAd(new AdRequest.Builder().build());
-        if (mInterstitialAd.isLoaded()) {
-            mInterstitialAd.show();
-        } else {
-            Log.d("TAG", "The interstitial wasn't loaded yet.");
-        }*/
-
+        },30,30, TimeUnit.SECONDS);
 
     }
 
@@ -95,5 +96,11 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra(URL,"https://www.w3schools.com/");
         }
         startActivity(intent);
+    }
+
+    public void prepareAd(){
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712"); //Interstitial ID
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
     }
 }
